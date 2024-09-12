c2c893d9a61764b25b6ab041998945fbb456610c
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2024-09-12 07:43:13 +0530

    basic invoice pdf generation

---
+++ lib/invoice_service.dart
@@ -0,0 +1,148 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:app_billgenerator/model/product.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:open_file/open_file.dart';
+import 'dart:io';
+
+class CustomRow {
+  final String itemName;
+  final String itemPrice;
+  final String amount;
+  final String total;
+  final String vat;
+
+  CustomRow(this.itemName, this.itemPrice, this.amount, this.total, this.vat);
+}
+
+class PdfInvoiceService {
+  Future<Uint8List> createHelloWorld() {
+    // @new:
+    final pdf = pw.Document();
+    pdf.addPage(pw.Page(build: (context) {
+      return pw.Center(
+        child: pw.Text('Hello World'),
+      );
+    }));
+    return pdf.save();
+
+    // @old:
+    // throw UnimplementedError();
+    // TODO 5 - 8 (Optional) only to showcase the pdf generation
+    // TODO 5: Create a pdf document
+    // TODO 6: Add a page
+    // TODO 7: Write Hello World in the Center
+    // TODO 8: Save the document and return the Uint8List
+  }
+
+  Future<Uint8List> createInvoice(List<Product> soldProducts) async {
+    // throw UnimplementedError();
+    // new:
+    final pdf = pw.Document();
+    final List<CustomRow> elements = [
+      CustomRow('Item Name', 'Item Price', 'Amount', 'Total', 'Vat'),
+      for (var product in soldProducts)
+        CustomRow(
+          product.name,
+          product.price.toStringAsFixed(2),
+          product.amount.toStringAsFixed(2),
+          (product.price * product.amount).toStringAsFixed(2),
+          (product.price / 100 * product.vatInPercent).toStringAsFixed(2),
+        ),
+      CustomRow(
+        "Sub Total",
+        "",
+        "",
+        "",
+        "Rs ${getVatTotal(soldProducts)}",
+      ),
+      CustomRow(
+        "VAT Total",
+        "",
+        "",
+        "",
+        "Rs ${getVatTotal(soldProducts)}",
+      ),
+      CustomRow(
+        "Total",
+        "",
+        "",
+        "",
+        "Rs ${getVatTotal(soldProducts)}",
+      ),
+    ];
+
+    final image = (await rootBundle.load('images/on_pdf/pdf_logo.jpg'))
+        .buffer
+        .asUint8List();
+    pdf.addPage(pw.Page(
+      pageFormat: PdfPageFormat.a4,
+      build: (context) {
+        return pw.Column(children: [
+          pw.Image(pw.MemoryImage(image),
+              width: 150, height: 150, fit: pw.BoxFit.cover),
+          pw.Row(
+              mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+              children: [
+                pw.Column(children: [
+                  pw.Text('Customer Name'),
+                  pw.Text('Customer Address'),
+                  pw.Text('Customer City'),
+                ]),
+                pw.Column(children: [
+                  pw.Text('Max Weber'),
+                  pw.Text('Weird Street Name 1'),
+                  pw.Text('70702 Not my city'),
+                  pw.Text('Invoice-Nr: 0001'),
+                ]),
+              ])
+        ]);
+      },
+    ));
+
+    return pdf.save();
+
+    // @old:
+    // TODO 13: Create a pdf document
+    // TODO 14: Create a list of "CustomRow" which contains each row of the item list
+    // TODO 15: Load the image via the rootBundle
+    // TODO 16: Add a page to the PDF and add one by one the elements
+    // TODO 17: Save the PDF and return the Uint8List
+  }
+
+  Future<void> savePdfFile(String fileName, Uint8List byteList) async {
+    // @new:
+    // throw UnimplementedError();
+    final output = await getTemporaryDirectory();
+    var filepath = '${output.path}/$fileName.pdf';
+    final file = File(filepath); // :-> NOTE: "not possible for Web"
+    await file.writeAsBytes(byteList); // byteList := "pdf"
+    await OpenFile.open(filepath);
+
+    // @old:
+    // TODO 9: Create a filepath where you want to save it (Flutter use getTemporaryDirectory())
+    // TODO 10: Create a File instance with the filepath
+    // TODO 11: Write into the file the byteArray
+    // TODO 12: Open the document with OpenDocument
+  }
+
+  String getSubTotal(List<Product> products) {
+    return products
+        .fold(0.0,
+            (double prev, element) => prev + (element.amount * element.price))
+        .toStringAsFixed(2);
+  }
+
+  String getVatTotal(List<Product> products) {
+    return products
+        .fold(
+          0.0,
+          (double prev, next) =>
+              prev + ((next.price / 100 * next.vatInPercent) * next.amount),
+        )
+        .toStringAsFixed(2);
+  }
+}
--- lib/pages/views/bills_view.dart
+++ lib/pages/views/bills_view.dart
@@ -1,7 +1,13 @@
+import 'dart:developer';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:app_billgenerator/invoice_service.dart';
+import 'package:app_billgenerator/model/product.dart';
 // import 'pdf_invoice';
+// import 'package:pdf/pdf.dart';
 
+// pdf:
 class BillsView extends StatefulWidget {
   const BillsView({super.key});
 
@@ -10,6 +16,8 @@ class BillsView extends StatefulWidget {
 }
 
 class _BillsViewState extends State<BillsView> {
+  final PdfInvoiceService service = PdfInvoiceService();
+
   @override
   Widget build(BuildContext context) {
     // return Container(
@@ -39,53 +47,71 @@ class _BillsViewState extends State<BillsView> {
     //     ],
     //   ),
     // );
+
+    final PdfInvoiceService service = PdfInvoiceService();
+    List<Product> soldProducts = [
+      Product('Membership', 999, 19),
+      Product('Nails', 252, 19),
+      Product('Hammer', 1999, 19),
+      Product('Pizza', 250, 7),
+    ];
+
     return Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFFF3E5F5), // Very light purple
-                Color(0xFFE8EAF6), // Very light indigo
-              ],
-            ),
-          ),
-          child: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // "old" :
-                // const Icon(Icons.inventory, color: Color(0xFF6A1B9A), size: 64),
-                // const SizedBox(height: 16),
-                // Text(
-                //   'Inventory Management',
-                //   style: GoogleFonts.merriweather(
-                //     fontSize: 24,
-                //     fontWeight: FontWeight.w500,
-                //     color: const Color(0xFF4A0E4E),
-                //   ),
-                // ),
-
-                // "new" :
-
-              ],
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFFF3E5F5), // Very light purple
+              Color(0xFFE8EAF6), // Very light indigo
+            ],
           ),
         ),
-        
-        
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            // @forPDF generator
-            // pdf-generator-stuff:
-            // final pdfFile = await PdfInVoic
+        child: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // "old" :
+              // const Icon(Icons.inventory, color: Color(0xFF6A1B9A), size: 64),
+              // const SizedBox(height: 16),
+              // Text(
+              //   'Inventory Management',
+              //   style: GoogleFonts.merriweather(
+              //     fontSize: 24,
+              //     fontWeight: FontWeight.w500,
+              //     color: const Color(0xFF4A0E4E),
+              //   ),
+              // ),
 
-          },
-          backgroundColor: Colors.deepPurple[300],
-          shape: const CircleBorder() ,
-          child: const Icon(Icons.add, size: 28,),
+              // "new" :
+            ],
+          ),
         ),
-        );
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // @forPDF generator
+          // pdf-generator-stuff:
+          // final data = await service.createHelloWorld();
+          final data = await service.createInvoice(soldProducts);
+          service.savePdfFile('Bill Name', data);
+        },
+        // backgroundColor: Colors.deepPurple[300],
+        shape: const CircleBorder(),
+        // child: const Icon(
+        //   Icons.add,
+        //   size: 28,
+        // ),
+        child: Center(
+          child: Image.asset(
+            'images/icons/pencil.png',
+            color: Colors.deepPurple[300],
+            height: 30,
+            width: 30,
+          ),
+        ),
+      ),
+    );
   }
 }
